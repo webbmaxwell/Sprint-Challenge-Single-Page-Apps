5f277d638a1108eddbd5bba1b1a1d7e09fccb855
webbmaxwell <webbwrites@gmail.com>
2019-08-17 15:54:48 -0600

    Refactored render keys to show return keywords

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -6,23 +6,27 @@ import { NavLink } from "react-router-dom";
 const panes = [
   { menuItem: 'Home Page',
     render: () => {
-      <Tab.Pane>Tab 1 Content</Tab.Pane>
-    }
+      return (
+        <Tab.Pane>Tab 1 Content</Tab.Pane>
+      )}
   },
   { menuItem: 'Characters',
     render: () => {
-      <Tab.Pane>Tab 2 Content</Tab.Pane>
-    }
+      return (
+        <Tab.Pane>Tab 2 Content</Tab.Pane>
+      )}
   },
   { menuItem: 'Locations',
     render: () => {
-      <Tab.Pane>Tab 3 Content</Tab.Pane>
-    }
+      return (
+        <Tab.Pane>Tab 3 Content</Tab.Pane>
+      )}
   },
   { menuItem: 'Episodes',
     render: () => {
-      <Tab.Pane>Tab 3 Content</Tab.Pane>
-    }
+      return (
+        <Tab.Pane>Tab 3 Content</Tab.Pane>
+      )}
   },
 ]
 
