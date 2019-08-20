65dab7d78f6133330d0289b9b09debb0aac5cf70
webbmaxwell <webbwrites@gmail.com>
2019-08-19 20:28:58 -0600

    Had to start refactoring because the Semantic UI components did not include Routes and Links :(

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import HomePage from './HomePage.js';
+import CharacterList from './CharacterList.js';
+import LocationsList from './LocationsList.js';
+import EpisodeList from './EpisodeList.js';
+
 
 function AppRouter() {
   return (
     <div className="">
+    <Route exact path="/" component={HomePage} />
+    <Route path="/character" component={CharacterList} />
+    <Route path="/location" component={LocationsList} />
+    <Route path="/episode" component={EpisodeList} />
     </div>
   )
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,39 +1,52 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
-// import { NavLink } from "react-router-dom";
+// import { Tab } from 'semantic-ui-react';
+import { NavLink } from "react-router-dom";
 
 import HomePage from './HomePage.js';
 import CharacterList from './CharacterList.js';
 import LocationsList from './LocationsList.js';
 import EpisodeList from './EpisodeList.js';
 
-const panes = [
-  { menuItem: 'Home Page',
-    render: () => {
-      return (
-        <Tab.Pane>{<HomePage />}</Tab.Pane>
-      )}
-  },
-  { menuItem: 'Characters',
-    render: () => {
-      return (
-        <Tab.Pane>{<CharacterList />}</Tab.Pane>
-      )}
-  },
-  { menuItem: 'Locations',
-    render: () => {
-      return (
-        <Tab.Pane>{<LocationsList />}</Tab.Pane>
-      )}
-  },
-  { menuItem: 'Episodes',
-    render: () => {
-      return (
-        <Tab.Pane>{<EpisodeList />}</Tab.Pane>
-      )}
-  },
-]
+function TabNav() {
+  return(
+    <nav>
+      <NavLink>Home Page</NavLink>
+      <NavLink>Characters</NavLink>
+      <NavLink>Locations</NavLink>
+      <NavLink>Episodes</NavLink>
+    </nav>
+  )
+}
 
-const TabExampleBasic = () => <Tab panes={panes} />
+export default TabNav;
 
-export default TabExampleBasic
+// const panes = [
+//   { menuItem: 'Home Page',
+//     render: () => {
+//       return (
+//         <Tab.Pane>{<HomePage />}</Tab.Pane>
+//       )}
+//   },
+//   { menuItem: 'Characters',
+//     render: () => {
+//       return (
+//         <Tab.Pane>{<CharacterList />}</Tab.Pane>
+//       )}
+//   },
+//   { menuItem: 'Locations',
+//     render: () => {
+//       return (
+//         <Tab.Pane>{<LocationsList />}</Tab.Pane>
+//       )}
+//   },
+//   { menuItem: 'Episodes',
+//     render: () => {
+//       return (
+//         <Tab.Pane>{<EpisodeList />}</Tab.Pane>
+//       )}
+//   },
+// ]
+
+// const TabExampleBasic = () => <Tab panes={panes} />
+
+// export default TabExampleBasic
